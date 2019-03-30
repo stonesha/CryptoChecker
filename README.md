0dc0cdcf5303a9030a5a34e766a37cb88b42533f
stonesha <stonesha@users.noreply.github.com>
2019-03-29 22:17:04 -0700

    Finished first basic version

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # cryptochecker
 Monitor cryptocurrencies in your desired real world currency.
 
-## Dependencies
+## Dependencies Used
 Node.js https://nodejs.org/en/ \
 Node Package Manager https://www.npmjs.com/get-npm \
 Electron API https://electronjs.org/docs/api \
 - Electron Reload \
 Glob https://www.npmjs.com/package/glob \
-Axios
+Axios https://www.npmjs.com/package/axios 
 
 ## To-Do
-- [ ] Integrate CoinMarketAPI
-- [ ] Get first version running
-- [ ] Add cryptocurrency monitoring
+- [x] Integrate CryptoCompare API
+- [x] Get first version running
+- [x] Add cryptocurrency monitoring
+- [ ] Add Top 10 Cryptocurrencies
+- [ ] Add Icons
 - [ ] Add more features (price tracking, graphs, etc.)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const axios = require('axios');
 
-var price = document.querySelector('h1')
+var BTCprice = document.querySelector('h1')
 var targetPrice = document.getElementById('targetPrice')
 
 function getBTC() {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
     .then(res => {
-        const cryptos = res.data.BTC.USD
-        price.innerHTML = '$'+cryptos.toLocaleString('en')
+        const BTC_cryptos = res.data.BTC.USD
+        BTC_price.innerHTML = '$'+BTC_cryptos.toLocaleString('en')
+    })
+} 
+
+function getETH(){
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD')
+    .then(res => {
+        const ETH_cryptos = res.data.ETH.USD
+        ETH_price.innerHTML = '$'+ETH_cryptos.toLocaleString('en')
     })
 }
 
 getBTC();
-setInterval(getBTC(), 30000);
+getETH();
