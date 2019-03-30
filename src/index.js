dfc8c9877c4fc043c191fe6a6d37cae6fdacc6c7
stonesha <stonesha@users.noreply.github.com>
2019-03-29 23:39:05 -0700

    Added final cryptos for now

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 var BTCprice = document.querySelector('h1')
 var targetPrice = document.getElementById('targetPrice')
 
+//bitcoin
 function getBTC() {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
     .then(res => {
@@ -11,6 +12,7 @@ function getBTC() {
     })
 } 
 
+//ethererum
 function getETH(){
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD')
     .then(res => {
@@ -19,6 +21,7 @@ function getETH(){
     })
 }
 
+//EOS
 function getEOS()
 {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=EOS&tsyms=USD')
@@ -28,6 +31,39 @@ function getEOS()
     })
 }
 
+//Litecoin
+function getLTC()
+{
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=LTC&tsyms=USD')
+    .then(res => {
+        const LTC_cryptos = res.data.LTC.USD
+        LTC_price.innerHTML = '$'+LTC_cryptos.toLocaleString('en')
+    })
+}
+
+//XRP
+function getXRP()
+{
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=XRP&tsyms=USD')
+    .then(res => {
+        const XRP_cryptos = res.data.XRP.USD
+        XRP_price.innerHTML = '$'+XRP_cryptos.toLocaleString('en')
+    })
+}
+
+//NEO
+function getNEO()
+{
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=NEO&tsyms=USD')
+    .then(res => {
+        const NEO_cryptos = res.data.NEO.USD
+        NEO_price.innerHTML = '$'+NEO_cryptos.toLocaleString('en')
+    })
+}
+
 getBTC();
 getETH();
 getEOS();
+getLTC();
+getXRP();
+getNEO();
